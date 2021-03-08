8b55326fefcfe24e826646367a92394fb44fd053
Leon <leongrinshpun@gmail.com>
2021-03-08 10:48:44 +0200

    Initial commit

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int score = 0;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag != "Hit")
+        {
+            score++;
+        }
+    }
+}
